7714d381236f7ee3bd5a4f790d2cb086c08c55af
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-11 12:57:51 +0900

    springboot 저장소초기화

---
+++ src/main/java/kimch321/springboot/semiprojcectv5/SemiProjcectv5Application.java
@@ -0,0 +1,13 @@
+package kimch321.springboot.semiprojcectv5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiProjcectv5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemiProjcectv5Application.class, args);
+    }
+
+}
